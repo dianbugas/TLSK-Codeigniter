120e98f17172e35f5b9b88e01257de371651e8f6
dianynf <dianbugas@gmail.com>
2019-01-31 22:20:13 +0700

    CRUD

--- application/views/suratmasuk/detail.php
+++ application/views/suratmasuk/detail.php
@@ -1,15 +1,42 @@
 <div class="container">
-    <div class="row-mt-3">
-        <div class="col-md-6">
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title"><?= $suratmasuk['nomor_surat']; ?></h5>
-                    <h6 class="card-subtitle mb-2 text-muted"><?= $suratmasuk['tanggal_surat']; ?></h6>
-                    <p class="card-text"><?= $suratmasuk['tanggal_terima']; ?></p>
-                    <p class="card-text"><?= $suratmasuk['dari']; ?></p>
-                    <a href="<?= base_url(); ?>suratmasuk/" class="card-link">Kembali</a>
-                </div>
-            </div>
+    <div class="row mt-4">
+        <div class="col md-6">
+            <h3>Data Surat Masuk</h3>
+            <table class="table table-striped table-bordered">
+                <tr>
+                    <th scope="col">Nomor Surat</th>
+                    <th scope="col"><?= $suratmasuk['nomor_surat']; ?></th>
+                </tr>
+                <tr>
+                    <th scope="col">Tanggal Surat</th>
+                    <th scope="col"><?= $suratmasuk['tanggal_surat']; ?></th>
+                </tr>
+                <tr>
+                    <th scope="col">Tanggal Terima</th>
+                    <th scope="col"><?= $suratmasuk['tanggal_terima']; ?></th>
+                </tr>
+                <tr>
+                    <th scope="col">Dari</th>
+                    <th scope="col"><?= $suratmasuk['dari']; ?></th>
+                </tr>
+                <tr>
+                    <th scope="col">Prihal</th>
+                    <th scope="col"><?= $suratmasuk['perihal']; ?></th>
+                </tr>
+                <tr>
+                    <th scope="col">Lampiran</th>
+                    <th scope="col"><?= $suratmasuk['lampiran']; ?></th>
+                </tr>
+                <tr>
+                    <th scope="col">Keterangan</th>
+                    <th scope="col"><?= $suratmasuk['keterangan']; ?></th>
+                </tr>
+                <tr>
+                    <th scope="col">Surat</th>
+                    <th scope="col"><?= $suratmasuk['surat']; ?></th>
+                </tr>
+            </table>
+            <a href="<?= base_url(); ?>suratmasuk/" class="card-link btn btn-primary">Kembali</a>
         </div>
     </div>
 </div>
