21b82dd27f68b7f1425768476505985358ed53b5
dianynf <dianbugas@gmail.com>
2019-02-02 09:23:27 +0700

    datepicker

--- application/views/suratmasuk/tambah.php
+++ application/views/suratmasuk/tambah.php
@@ -1,50 +1,61 @@
 <div class="container">
-    <div class="row-mt-3">
-        <div class="col-md-8">
+    <div class="row mt-3">
+        <div class="col-md-10">
             <div class="card">
                 <div class="card-header">
-                    Form Tambah Data Mahasiswa
+                    Form Tambah Data Surat Masuk
                 </div>
                 <div class="card-body">
                     <form action="" method='post'>
-                        <div class="form-group">
-                            <label for="nomor_surat">Nomor Surat</label>
-                            <input type="text" name="nomor_surat" class="form-control" id="nomor_surat">
+                        <div class="form-group row">
+                            <label for="nomor_surat" class="col-sm-2 col-form-label">Nomor Surat</label>
+                            <div class="col-sm-7">
+                            <input type="text" name="nomor_surat" class="form-control" id="nomor_surat" placeholder="Nomor Surat" width="260">
                             <small class="form-text text-danger"><?= form_error('nomor_surat'); ?></small>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="tanggal_surat">Tanggal Surat</label>
-                            <div>
-                            <input type="text" name="tanggal_surat" class="form-control" id="datepicker4" width="300">
+                        <div class="form-group row">
+                            <label for="tanggal_surat" class="col-sm-2 col-form-label">Tanggal Surat</label>
+                            <div class="col-sm-7">
+                            <input type="text" name="tanggal_surat" class="form-control" id="datepicker4" width="260" placeholder="Tanngal Surat">
                             <small class="form-text text-danger"><?= form_error('tanggal_surat'); ?></small>
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label for="tanggal_terima">Tanggal Terima</label>
-                            <input type="text" name="tanggal_terima" class="form-control" id="datepicker3" width="300">
+                        <div class="form-group row">
+                            <label for="tanggal_terima" class="col-sm-2 col-form-label">Tanggal Terima</label>
+                            <div class="col-sm-7">
+                            <input type="text" name="tanggal_terima" class="form-control" id="datepicker3" width="260" placeholder="Tanggal Terima">
                             <small class="form-text text-danger"><?= form_error('tanggal_terima'); ?></small>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="dari">Dari</label>
-                            <input type="text" name="dari" class="form-control" id="dari">
+                        <div class="form-group row">
+                            <label for="dari" class="col-sm-2 col-form-label">Dari</label>
+                            <div class="col-sm-7">
+                            <input type="text" name="dari" class="form-control" id="dari" placeholder="Dari" width="260">
                             <small class="form-text text-danger"><?= form_error('dari'); ?></small>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="perihal">Perihal</label>
-                            <input type="text" name="perihal" class="form-control" id="perihal">
+                        <div class="form-group row">
+                            <label for="perihal" class="col-sm-2 col-form-label">Perihal</label>
+                            <div class="col-sm-7">
+                            <input type="text" name="perihal" class="form-control" id="perihal" placeholder="Perihal" width="260">
                             <small class="form-text text-danger"><?= form_error('perihal'); ?></small>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="lampiran">Lampiran</label>
-                            <input type="text" name="lampiran" class="form-control" id="lampiran">
+                        <div class="form-group row">
+                            <label for="lampiran" class="col-sm-2 col-form-label">Lampiran</label>
+                            <div class="col-sm-7">
+                            <input type="text" name="lampiran" class="form-control" id="lampiran" placeholder="Lampiran" width="260">
                             <small class="form-text text-danger"><?= form_error('lampiran'); ?></small>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="surat">Surat</label>
-                            <input type="file" name="surat" class="form-control" id="surat">
-                            <small class="form-text text-danger"><?= form_error('surat'); ?></small>
+                        <div class="form-group row">
+                                <label for="surat" class="col-sm-2 col-form-label">Surat</label>
+                                <div class="col-sm-7">
+                                <input type="file" class="form-control-file" id="exampleFormControlFile1" name="surat" id="surat">
+                                <small class="form-text text-danger"><?= form_error('surat'); ?></small>
+                                </div>
                         </div>
-
                         <button type="submit" name="tambah" class="btn btn-primary float-right">Tambah Data</button>
                     </form>
                 </div>
