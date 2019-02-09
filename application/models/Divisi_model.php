34598aea2f6fdb856d7a48af8163f50be2b44c31
dianynf <dianbugas@gmail.com>
2019-02-09 15:18:41 +0700

    divisi Model

--- application/models/Divisi_model.php
+++ application/models/Divisi_model.php
@@ -5,4 +5,34 @@ class Divisi_model extends CI_Model
     {
         return $this->db->get('divisi')->result_array();
     }
+
+    public function tambahDataDivisi()
+    {
+        $data = [
+            "divisi" => $this->input->post('divisi', true),
+            "nama" => $this->input->post('nama', true),
+        ];
+        $this->db->insert('divisi', $data);
+    }
+
+    public function editDataDivisi($id)
+    {
+        $data = [
+            "divisi" => $this->input->post('divisi', true),
+            "nama" => $this->input->post('nama', true),
+        ];
+
+        $this->db->where('id', $this->input->post('id'));
+        $this->db->update('divisi', $data);
+    }
+
+    public function getDivisiById($id)
+    {
+        return $this->db->get_where('divisi', ['id' => $id])->row_array();
+    }
+
+    public function hapusDataDivisi($id)
+    {
+        $this->db->delete('divisi', ['id' => $id]);
+    }
 }
