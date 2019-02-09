60b55ec10b222ce7eace7ef0986ab7620cc3607f
dianynf <dianbugas@gmail.com>
2019-02-09 15:12:05 +0700

    divisi controller

--- application/controllers/Divisi.php
+++ application/controllers/Divisi.php
@@ -23,7 +23,54 @@ class Divisi extends CI_Controller
     public function tambah()
     {
         $data['judul'] = 'Tambah Data Divisi';
+        $this->form_validation->set_rules('divisi', 'Divisi', 'required');
+        $this->form_validation->set_rules('nama', 'Nama', 'required');
 
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('divisi/tambah');
+            $this->load->view('templates/footer');
+        } else {
+            $this->Suratkeluar_model->tambahDataDivisi();
+            $this->session->set_flashdata('flash', 'Ditambahkan');
+            redirect('divisi');
+        }
+
+    }
+
+    public function edit($id)
+    {
+        $data['judul'] = 'Edit Data Divisi';
+        $data['divisi'] = $this->Divisi_model->getDivisiById($id);
+        $this->form_validation->set_rules('divisi', 'Divisi', 'required');
+        $this->form_validation->set_rules('nama', 'Nama', 'required');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('divisi/edit', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $this->Suratmasuk_model->editDataDivisi($id);
+            $this->session->set_flashdata('flash', 'Diubah');
+            redirect('divisi');
+        }
+    }
+
+    public function detail($id)
+    {
+        $data['judul'] = 'Detail Divisi';
+        $data['divisi'] = $this->Divisi_model->getDivisiById($id);
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('divisi/detail', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function hapus($id)
+    {
+        $this->Suratkeluar_model->hapusDataDivisi($id);
+        $this->session->set_flashdata('flash', 'Dihapus');
+        redirect('divisi');
     }
 
 }
