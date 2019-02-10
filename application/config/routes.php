25f25fc2b93344a5ba07f7c8a6fcf6ae629d3048
dianynf <dianbugas@gmail.com>
2019-02-10 10:08:54 +0700

    configurasi users

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
  */
+
+$route['profile'] = 'user/profile';
+
+$route['register'] = 'auth/register';
+$route['login'] = 'auth/login';
+$route['logout'] = 'auth/logout';
+$route['verify/(:any)/(:any)'] = 'auth/verify_register/$1/$1';
 $route['upload'] = 'Upload';
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = false;
