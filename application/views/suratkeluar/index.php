ea2542e4a114e503d5d6dd1ae965af7d525714b0
dianynf <dianbugas@gmail.com>
2019-02-13 23:49:42 +0700

    view

--- application/views/suratkeluar/index.php
+++ application/views/suratkeluar/index.php
@@ -30,46 +30,46 @@
 
     <div class="row mt-4">
         <div class="col md-6">
-        <div class="table-responsive-sm">
-            <h3>Daftar Surat Keluar</h3>
-            <?php if (empty($suratkeluar)) : ?>
-                <div class="alert alert-danger" role="alert">
-                    Data Surat Keluar tidak ditemukan
-                </div>
-            <?php endif; ?>
-            <table class="table table-striped table-bordered">
-                <thead class="thead-dark">
-                    <tr>
-                        <th scope="col md-3" class="text-center">no</th>
-                        <th scope="col" class="text-center">Status</th>
-                        <th scope="col" class="text-center">Nomor Surat</th>
-                        <th scope="col" class="text-center">Tanggal Surat</th>
-                        <th scope="col" class="text-center">Dari</th>
-                        <th scope="col" class="text-center">Prihal</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $no = 1;
-                    foreach ($suratkeluar as $sk) : ?>
-                    <tr>
-                        <th class="text-center"><?= $no++; ?></th>
-                        <th>
-                            <a href="#" class="fa fa-download fa-lg" style="color:black"></a>
-                            <a href="<?= base_url(); ?>suratkeluar/detail/<?= $sk['id']; ?>" class="fa fa-search-plus fa-lg"></a>
-                            <a href="<?= base_url(); ?>suratkeluar/edit/<?= $sk['id']; ?>" class="fa fa-pencil-square-o fa-lg" style="color:Green"></a>
-                            <a href="<?= base_url(); ?>suratkeluar/hapus/<?= $sk['id']; ?>" class="fa fa-trash fa-lg tombol-hapus" style="color:red"></a>
-                        </th>
-                        <th><?= $sk['nomor_surat']; ?></th> 
-                        <th><?= $sk['tanggal_surat']; ?></th>
-                        <th><?= $sk['dari']; ?></th>
-                        <th><?= $sk['kepada']; ?></th>   
-                    </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+            <div class="table-responsive-sm">
+                <h3>Daftar Surat Keluar</h3>
+                <?php if (empty($suratkeluar)) : ?>
+                    <div class="alert alert-danger" role="alert">
+                        Data Surat Keluar tidak ditemukan
+                    </div>
+                <?php endif; ?>
+                <table class="table table-striped table-bordered">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col md-3" class="text-center">no</th>
+                            <th scope="col" class="text-center">Status</th>
+                            <th scope="col" class="text-center">Nomor Surat</th>
+                            <th scope="col" class="text-center">Tanggal Surat</th>
+                            <th scope="col" class="text-center">Dari</th>
+                            <th scope="col" class="text-center">Prihal</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $no = 1;
+                        foreach ($suratkeluar as $sk) : ?>
+                        <tr>
+                            <th class="text-center"><?= $no++; ?></th>
+                            <th>
+                                <a href="#" class="fa fa-download fa-lg" style="color:black"></a>
+                                <a href="<?= base_url(); ?>suratkeluar/detail/<?= $sk['id']; ?>" class="fa fa-search-plus fa-lg"></a>
+                                <a href="<?= base_url(); ?>suratkeluar/edit/<?= $sk['id']; ?>" class="fa fa-pencil-square-o fa-lg" style="color:Green"></a>
+                                <a href="<?= base_url(); ?>suratkeluar/hapus/<?= $sk['id']; ?>" class="fa fa-trash fa-lg tombol-hapus" style="color:red"></a>
+                            </th>
+                            <th><?= $sk['nomor_surat']; ?></th> 
+                            <th><?= $sk['tanggal_surat']; ?></th>
+                            <th><?= $sk['dari']; ?></th>
+                            <th><?= $sk['kepada']; ?></th>   
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
     </div>
 </div>
 
--- application/views/suratmasuk/index.php
+++ application/views/suratmasuk/index.php
@@ -30,46 +30,46 @@
 
     <div class="row mt-4">
         <div class="col md-6">
-        <div class="table-responsive-sm">
-            <h3>Daftar Surat Masuk</h3>
-            <?php if (empty($suratmasuk)) : ?>
-                <div class="alert alert-danger" role="alert">
-                    Data Mahasiswa tidak ditemukan
-                </div>
-            <?php endif; ?>
-            <table class="table table-striped table-bordered">
-                <thead class="thead-dark">
-                    <tr>
-                        <th scope="col md-3" class="text-center">no</th>
-                        <th scope="col" class="text-center">Status</th>
-                        <th scope="col" class="text-center">Nomor Surat</th>
-                        <th scope="col" class="text-center">Tanggal Surat</th>
-                        <th scope="col" class="text-center">Dari</th>
-                        <th scope="col" class="text-center">Prihal</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $no = 1;
-                    foreach ($suratmasuk as $sm) : ?>
-                    <tr>
-                        <th class="text-center"><?= $no++; ?></th>
-                        <th>
-                            <a href="#" class="fa fa-download fa-lg" style="color:black"></a>
-                            <a href="<?= base_url(); ?>suratmasuk/detail/<?= $sm['id']; ?>" class="fa fa-search-plus fa-lg"></a>
-                            <a href="<?= base_url(); ?>suratmasuk/edit/<?= $sm['id']; ?>" class="fa fa-pencil-square-o fa-lg" style="color:Green"></a>
-                            <a href="<?= base_url(); ?>suratmasuk/hapus/<?= $sm['id']; ?>" class="fa fa-trash fa-lg tombol-hapus" style="color:red"></a>
-                        </th>
-                        <th><?= $sm['nomor_surat']; ?></th> 
-                        <th><?= $sm['tanggal_surat']; ?></th>
-                        <th><?= $sm['dari']; ?></th>
-                        <th><?= $sm['perihal']; ?></th>   
-                    </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+            <div class="table-responsive-sm">
+                <h3>Daftar Surat Masuk</h3>
+                <?php if (empty($suratmasuk)) : ?>
+                    <div class="alert alert-danger" role="alert">
+                        Data Mahasiswa tidak ditemukan
+                    </div>
+                <?php endif; ?>
+                <table class="table table-striped table-bordered">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col md-3" class="text-center">no</th>
+                            <th scope="col" class="text-center">Status</th>
+                            <th scope="col" class="text-center">Nomor Surat</th>
+                            <th scope="col" class="text-center">Tanggal Surat</th>
+                            <th scope="col" class="text-center">Dari</th>
+                            <th scope="col" class="text-center">Prihal</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $no = 1;
+                        foreach ($suratmasuk as $sm) : ?>
+                        <tr>
+                            <th class="text-center"><?= $no++; ?></th>
+                            <th>
+                                <a href="#" class="fa fa-download fa-lg" style="color:black"></a>
+                                <a href="<?= base_url(); ?>suratmasuk/detail/<?= $sm['id']; ?>" class="fa fa-search-plus fa-lg"></a>
+                                <a href="<?= base_url(); ?>suratmasuk/edit/<?= $sm['id']; ?>" class="fa fa-pencil-square-o fa-lg" style="color:Green"></a>
+                                <a href="<?= base_url(); ?>suratmasuk/hapus/<?= $sm['id']; ?>" class="fa fa-trash fa-lg tombol-hapus" style="color:red"></a>
+                            </th>
+                            <th><?= $sm['nomor_surat']; ?></th> 
+                            <th><?= $sm['tanggal_surat']; ?></th>
+                            <th><?= $sm['dari']; ?></th>
+                            <th><?= $sm['perihal']; ?></th>   
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
     </div>
 </div>
 
