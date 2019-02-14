82299da696a7505f83411ac4b23c6d54e881a0d2
dianynf <dianbugas@gmail.com>
2019-02-14 12:42:52 +0700

    view

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -38,18 +38,18 @@
                                         a_p = "AM";
                                     } else {
                                         a_p = "PM";
-                                    }
-                                    if (curr_hour == 0) {
-                                        curr_hour = 24;
-                                    }
-                                    if (curr_hour > 24) {
-                                        curr_hour = curr_hour - 12;
-                                    }
-                                    curr_hour = checkTime(curr_hour);
-                                    curr_minute = checkTime(curr_minute);
-                                    curr_second = checkTime(curr_second);
-                                    document.getElementById('clock').innerHTML = curr_hour + ":" + curr_minute + ":" + curr_second + " " + a_p;
-                                    }
+                                }
+                                if (curr_hour == 0) {
+                                    curr_hour = 24;
+                                }
+                                if (curr_hour > 24) {
+                                    curr_hour = curr_hour - 12;
+                                }
+                                curr_hour = checkTime(curr_hour);
+                                curr_minute = checkTime(curr_minute);
+                                curr_second = checkTime(curr_second);
+                                document.getElementById('clock').innerHTML = curr_hour + ":" + curr_minute + ":" +curr_second + " " + a_p;
+                                }
                                 function checkTime(i) {
                                     if (i < 10) {
                                     i = "0" + i;
