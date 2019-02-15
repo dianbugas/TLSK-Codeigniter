948fef9905dc7a42cfe5e38978798233ca00be49
dianynf <dianbugas@gmail.com>
2019-02-15 22:37:39 +0700

    update

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -9,37 +9,35 @@
     <script src="<?= base_url(); ?>assets/plugin/js/bootstrap-datepicker.min.js"></script>
     <script src="<?= base_url(); ?>assets/js/myscript.js"></script>    
     <script type="text/javascript">
-            function showTime() {
-                    var a_p = "";
-                    var today = new Date();
-                    var curr_hour = today.getHours();
-                    var curr_minute = today.getMinutes();
-                    var curr_second = today.getSeconds();
-                    if (curr_hour < 12) {
-                        a_p = "AM";
-                    } else {
-                        a_p = "PM";
-                    }
-                    if (curr_hour == 0) {
-                        curr_hour = 24;
-                    }
-                    if (curr_hour > 24) {
-                        curr_hour = curr_hour - 12;
-                    }
-                    curr_hour = checkTime(curr_hour);
-                    curr_minute = checkTime(curr_minute);
-                    curr_second = checkTime(curr_second);
-                    document.getElementById('clock').innerHTML = curr_hour + ":" + curr_minute + ":" + curr_second + " " + a_p;
+        function showTime() {
+                var a_p = "";
+                var today = new Date();
+                var curr_hour = today.getHours();
+                var curr_minute = today.getMinutes();
+                var curr_second = today.getSeconds();
+                if (curr_hour < 12) {
+                    a_p = "AM";
+                } else {
+                    a_p = "PM";
                 }
-
-                function checkTime(i) {
-                    if (i < 10) {
-                        i = "0" + i;
-                    }
-                    return i;
+                if (curr_hour == 0) {
+                    curr_hour = 24;
                 }
-                setInterval(showTime, 500);
-               
-        </script>
+                if (curr_hour > 24) {
+                    curr_hour = curr_hour - 12;
+                }
+                curr_hour = checkTime(curr_hour);
+                curr_minute = checkTime(curr_minute);
+                curr_second = checkTime(curr_second);
+                document.getElementById('clock').innerHTML = curr_hour + ":" + curr_minute + ":" + curr_second + " " + a_p;
+            }
+            function checkTime(i) {
+                if (i < 10) {
+                    i = "0" + i;
+                }
+                return i;
+            }
+            setInterval(showTime, 500);
+    </script>
 </body>
 </html>
